bdce4a19244d48d5ecd07dc7c3181db60830bbd2
Ivan <capobianco.ivan@gmail.com>
2022-12-14 18:27:25 +0100

    refactor: utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,17 +1,4 @@
-const normaliseHex = (raw: any) => {
-  const value = raw.replace(/\s/g, "");
-  return value.toUpperCase();
-};
-
-// const byteArrayToWordArray = (byteArray: any) => {
-//   const wordArray: any[] = [];
-
-//   for (let i = 0; i < byteArray.length; i++) {
-//     wordArray[(i / 4) | 0] |= byteArray[i] << (24 - 8 * i);
-//   }
-
-//   return CryptoJS.lib.WordArray.create(wordArray, byteArray.length);
-// };
+import { BigInteger } from "big-integer";
 
 const hexToBytes = (hexString: string): number[] => {
   const result = [];
@@ -24,8 +11,9 @@ const hexToBytes = (hexString: string): number[] => {
   return result;
 };
 
-const bytesToHex = (bytes: any): string => {
+const bytesToHex = (bytes: number[]): string => {
   const hex = [];
+
   for (let i = 0; i < bytes.length; i++) {
     hex.push((bytes[i] >>> 4).toString(16));
     hex.push((bytes[i] & 0xf).toString(16));
@@ -34,11 +22,11 @@ const bytesToHex = (bytes: any): string => {
   return hex.join("");
 };
 
-const bigIntegerToHexString = (bigInteger: any): string => {
+const bigIntegerToHexString = (bigInteger: BigInteger): string => {
   return bigInteger.toString(16);
 };
 
-const toAsciiCodes = (text: any) => {
+const toAsciiCodes = (text: string) => {
   const bytes = [];
 
   for (let i = 0; i < text.length; i++) {
@@ -46,26 +34,6 @@ const toAsciiCodes = (text: any) => {
   }
 
   return bytes;
-};
-
-const toHexDated = (number: number, octets: any): string => {
-  let hex = ("0" + Number(number).toString(16)).toUpperCase();
-
-  if (hex.length % 2 == 1) {
-    hex = "0" + hex;
-  }
-
-  const octet_count = hex.length / 2;
-  if (octet_count < octets) {
-    const added_zeroes = octets - octet_count;
-    for (let i = 0; i < added_zeroes; i++) {
-      hex = "00" + hex;
-    }
-  } else if (octet_count > octets) {
-    // hex = hex.substring((octet_count - octets) * 2, hex_chars.length);
-  }
-
-  return hex;
 };
 
 const toHex = (intNumber: number, octets: number): string => {
@@ -104,35 +72,35 @@ const u8AToHexString = (u8a: Uint8Array): string => {
   let hex = "";
 
   for (let i = 0; i < u8a.length; i++) {
-    let hex_pair = "0" + u8a[i].toString(16);
-    if (hex_pair.length == 3) {
-      hex_pair = hex_pair.substring(1, 3);
+    let hexPair = "0" + u8a[i].toString(16);
+    if (hexPair.length == 3) {
+      hexPair = hexPair.substring(1, 3);
     }
-    hex = hex + hex_pair;
+    hex = hex + hexPair;
   }
 
   return hex;
 };
 
-// Convert an integer to a hex string, padded with a leading zero if required;
+// Convert an integer to a hex string, padded with a leading zero if required
 const intToHex = (number: number): string => {
   const hex = "00" + number.toString(16);
   return hex.substring(hex.length - 2);
 };
 
-const xorU8Array = (bytes1: any, bytes2: any): Uint8Array => {
+const xorU8Array = (bytes1: Uint8Array, bytes2: Uint8Array): Uint8Array => {
   // Uint8Arrays in and out
   if (bytes1.length != bytes2.length) {
     console.log("ERROR in xorU8Array: operands must be the same length");
     return new Uint8Array([]);
   }
 
-  const xor_bytes = [];
+  const xorBytes = [];
   for (let i = 0; i < bytes1.length; i++) {
-    xor_bytes.push(bytes1[i] ^ bytes2[i]);
+    xorBytes.push(bytes1[i] ^ bytes2[i]);
   }
 
-  return new Uint8Array(xor_bytes);
+  return new Uint8Array(xorBytes);
 };
 
 const leastSignificantBit = (number: number): number => {
@@ -140,13 +108,10 @@ const leastSignificantBit = (number: number): number => {
 };
 
 const utils = {
-  normaliseHex,
-  // byteArrayToWordArray,
   hexToBytes,
   bytesToHex,
   bigIntegerToHexString,
   toAsciiCodes,
-  toHexDated,
   toHex,
   hexToU8A,
   u8AToHexString,
